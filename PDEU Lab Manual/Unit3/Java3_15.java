41fff91c3c4e7d620f0cc552c9775fd4bb08af59
Tirth Shah <cptirthp.shah@gmail.com>
2023-11-04 23:49:24 +0530

    Updated PDEU Lab Manual

---
+++ PDEU Lab Manual/Unit3/Java3_15.java
@@ -0,0 +1,96 @@
+public class Java3_15 {
+    
+    public static void main(String[] args) {
+
+        int[] arr = { 2, 4, 6, 8, 10, 12, 14 };
+
+        int key = 7;
+
+        try {
+
+            int index = binarySearch(arr, key);
+
+            if (index != -1) {
+                System.out.println("Element " + key + " found at index " + index);
+            } 
+            
+            else {
+                System.out.println("Element " + key + " not found in the array.");
+            }
+
+        } 
+        
+        catch (ArrayIndexOutOfBoundsException e) {
+            System.out.println("ArrayIndexOutOfBoundsException in correct binary search: " + e);
+        }
+
+        try {
+
+            int index = wrongBinarySearch(arr, key);
+
+            if (index != -1) {
+                System.out.println("Element " + key + " found at index " + index);
+            } 
+            
+            else {
+                System.out.println("Element " + key + " not found in the array.");
+            }
+
+        } 
+        
+        catch (ArrayIndexOutOfBoundsException e) {
+            System.out.println("ArrayIndexOutOfBoundsException in wrong binary search: " + e);
+        }
+
+    }
+
+    public static int binarySearch(int[] arr, int key) {
+        
+        int low = 0;
+        int high = arr.length - 1;
+
+        while (low <= high) {
+            
+            int mid = low + (high - low) / 2;
+
+            if (arr[mid] == key) {
+                return mid;
+            } 
+            
+            else if (arr[mid] < key) {
+                low = mid + 1;
+            } 
+            
+            else {
+                high = mid - 1;
+            }
+        }
+
+        return -1;
+    }
+
+    public static int wrongBinarySearch(int[] arr, int key) {
+
+        int low = 0;
+        int high = arr[arr.length]; //Intentionally making error
+
+        while (low <= high) {
+            
+            int mid = low + (high - low) / 2;
+
+            if (arr[mid] == key) {
+                return mid;
+            } 
+            
+            else if (arr[mid] < key) {
+                low = mid + 1;
+            } 
+            
+            else {
+                high = mid - 1;
+            }
+        }
+
+        return -1;
+    }
+}
