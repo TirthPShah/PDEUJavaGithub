ad3b3b9234e8a18729524af75514a9d643eabaab
Tirth Shah <cptirthp.shah@gmail.com>
2023-11-05 15:36:08 +0530

    Updated PDEU Lab Manual

---
+++ PDEU Lab Manual/Unit4/Java4_3.java
@@ -0,0 +1,109 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+import java.io.BufferedReader;
+import java.io.FileReader;
+
+public class Java4_3 {
+
+    private JFrame frame;
+    private JTextArea codeArea;
+
+    public Java4_3() {
+
+        frame = new JFrame("Java4_3");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setSize(800, 600);
+
+        JMenuBar menuBar = new JMenuBar();
+
+        int[] noOfProg = {8, 3, 15, 6};
+
+        for (int unit = 1; unit <= noOfProg.length; unit++) {
+
+            JMenu unitMenu = new JMenu("Unit " + unit);
+
+            for (int prog = 1; prog <= noOfProg[unit - 1]; prog++) {
+
+                String progName = "Java" + unit + "_" + prog;
+
+                JMenuItem menuItem = new JMenuItem("Open " + progName);
+                String actionCommand = "Unit" + unit + " " + progName;
+
+                menuItem.setActionCommand(actionCommand);
+                menuItem.addActionListener(new MenuItemListener());
+
+                unitMenu.add(menuItem);
+            }
+
+            menuBar.add(unitMenu);
+        }
+
+        frame.setJMenuBar(menuBar);
+
+        codeArea = new JTextArea();
+        codeArea.setEditable(false);
+        codeArea.setFont(new Font("Courier New", Font.PLAIN, 14));
+
+        JScrollPane scrollPane = new JScrollPane(codeArea);
+        frame.add(scrollPane, BorderLayout.CENTER);
+
+        frame.setVisible(true);
+    }
+
+    class MenuItemListener implements ActionListener {
+
+        public void actionPerformed(ActionEvent e) {
+
+            String actionCommand = e.getActionCommand();
+
+            String[] parts = actionCommand.split(" ");
+
+            String unitName = parts[0];
+            String lessonName = parts[1];
+            String fileName = "../" + unitName + "/" + lessonName + ".java";
+
+            BufferedReader reader = null;
+
+            try {
+
+                reader = new BufferedReader(new FileReader(fileName));
+
+                codeArea.setText("");
+
+                String line = reader.readLine();
+
+                while (line != null) {
+                    
+                    codeArea.append(line);
+                    codeArea.append("\n");
+                    line = reader.readLine();
+
+                }
+
+            } 
+            
+            catch (Exception er) {
+                codeArea.setText(er.toString());
+            }
+
+            finally {
+
+                try {
+                    reader.close();
+                }
+
+                catch (Exception er) {
+                    codeArea.setText(er.toString());
+                }
+            }
+        }
+    }
+    
+
+    public static void main(String[] args) {
+        SwingUtilities.invokeLater(() -> {
+            new Java4_3();
+        });
+    }
+}
---
+++ PDEU Lab Manual/Unit4/Java4_4.java
@@ -0,0 +1,142 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+
+public class Java4_4 {
+
+    private JFrame frame;
+    private JTextField display;
+
+    private char operator = ' ';
+
+    private boolean isNewNumber;
+
+    private double firstNumber;
+    private double secondNumber;
+
+    public Java4_4() {
+
+        frame = new JFrame("Java4_4");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setLayout(new BorderLayout());
+
+        display = new JTextField();
+        display.setEditable(false);
+        display.setFont(new Font("Arial", Font.PLAIN, 20));
+        frame.add(display, BorderLayout.NORTH);
+
+        JPanel buttonPanel = new JPanel(new GridLayout(4, 3));
+        
+        String[] buttons = {
+            "7", "8", "9",
+            "4", "5", "6",
+            "1", "2", "3",
+            "C", "0", "="
+        };
+
+        for (String button : buttons) {
+
+            JButton b = new JButton(button);
+            b.addActionListener(new ButtonListener());
+            buttonPanel.add(b);
+
+        }
+
+        frame.add(buttonPanel, BorderLayout.CENTER);
+
+        JPanel operationPanel = new JPanel(new FlowLayout());
+
+        String[] operationButtons = {"+", "-", "*", "/"};
+
+        for (String button : operationButtons) {
+
+            JButton b = new JButton(button);
+            b.addActionListener(new ButtonListener());
+            operationPanel.add(b);
+
+        }
+
+        frame.add(operationPanel, BorderLayout.SOUTH);
+
+        frame.pack();
+        frame.setVisible(true);
+    }
+
+    class ButtonListener implements ActionListener {
+
+        public void actionPerformed(ActionEvent e) {
+
+            String command = e.getActionCommand();
+
+            if (command.equals("0") || command.equals("1") || command.equals("2") || command.equals("3") || command.equals("4") || command.equals("5") || command.equals("6") || command.equals("7") || command.equals("8") || command.equals("9")) {
+
+                if (isNewNumber) {
+                    display.setText(command);
+                    isNewNumber = false;
+                } 
+                
+                else {
+                    display.setText(display.getText() + command);
+                }
+
+            } 
+            
+            else if (command.equals("+") || command.equals("-") || command.equals("*") || command.equals("/")) {
+                
+                firstNumber = Double.parseDouble(display.getText());
+                operator = command.charAt(0);
+                isNewNumber = true;
+
+            } 
+            
+            else if (command.equals("C")) {
+                
+                display.setText("");
+                firstNumber = 0;
+                operator = ' ';
+
+            } 
+            
+            else if (command.equals("=")) {
+
+                secondNumber = Double.parseDouble(display.getText());
+
+                switch (operator) {
+
+                case '+':
+                    display.setText(Double.toString(firstNumber + secondNumber));
+                break;
+
+                case '-':
+                    display.setText(Double.toString(firstNumber - secondNumber));
+                break;
+
+                case '*':
+                    display.setText(Double.toString(firstNumber * secondNumber));
+                break;
+
+                case '/':
+
+                    if (secondNumber != 0) {
+                        display.setText(Double.toString(firstNumber / secondNumber));
+                        break;
+                    } 
+                    
+                    else {
+                        display.setText(Double.toString(Double.NaN));
+                        break;
+                    }
+
+                }
+
+                isNewNumber = true;
+            }
+
+        }
+
+    }
+
+    public static void main(String[] args) {
+        new Java4_4();
+    }
+}
