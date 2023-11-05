bd379445cfce57513018d2faba1029364ab90135
Tirth Shah <cptirthp.shah@gmail.com>
2023-11-05 16:29:28 +0530

    Updated PDEU Lab Manual

---
+++ PDEU Lab Manual/Unit4/Java4_5.java
@@ -0,0 +1,45 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+
+public class Java4_5 {
+
+    private JFrame frame;
+    private JTextField nameField; // Declare nameField as an instance variable
+
+    public Java4_5() {
+
+        frame = new JFrame("Java4_5");
+        frame.setLayout(new BorderLayout());
+
+        JPanel form = new JPanel(new GridLayout(1, 2));
+
+        JLabel nameLabel = new JLabel("Name: ");
+        nameField = new JTextField(10); // Assign the instance variable
+
+        form.add(nameLabel);
+        form.add(nameField);
+
+        frame.add(form, BorderLayout.CENTER);
+
+        JButton button = new JButton("Submit");
+        button.addActionListener(new DialogueOpener());
+
+        frame.add(button, BorderLayout.SOUTH);
+
+        frame.setSize(300, 100);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setVisible(true);
+    }
+
+    class DialogueOpener implements ActionListener {
+        public void actionPerformed(ActionEvent e) {
+            String name = nameField.getText();
+            JOptionPane.showMessageDialog(frame, "Hello, " + name + "!");
+        }
+    }
+
+    public static void main(String[] args) {
+        new Java4_5();
+    }
+}
