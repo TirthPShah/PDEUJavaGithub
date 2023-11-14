6cb74fe5f16ed3cf384a4972388ba9cccd25983b
Tirth Shah <cptirthp.shah@gmail.com>
2023-11-14 20:05:10 +0530

    Updated PDEU Lab Manual

--- PDEU Lab Manual/Unit4/Java4_5.java
+++ PDEU Lab Manual/Unit4/Java4_5.java
@@ -12,7 +12,7 @@ public class Java4_5 implements ActionListener {
         frame = new JFrame("Java4_5");
         frame.setLayout(new BorderLayout());
 
-        JPanel form = new JPanel(new GridLayout(1, 2));
+        JPanel form = new JPanel(new GridLayout(2, 2));
 
         JLabel nameLabel = new JLabel("Name: ");
         nameField = new JTextField(10);
@@ -20,21 +20,37 @@ public class Java4_5 implements ActionListener {
         form.add(nameLabel);
         form.add(nameField);
 
+        JButton button1 = new JButton("Create Dialogue Box 1");
+        button1.setActionCommand("Button1");
+        button1.addActionListener(this);
+
+        JButton button2 = new JButton("Create Dialogue Box 2");
+        button2.setActionCommand("Button2");
+        button2.addActionListener(this);
+
+        form.add(button1);
+        form.add(button2);
+
         frame.add(form, BorderLayout.CENTER);
 
-        JButton button = new JButton("Submit");
-        button.addActionListener(this);
-
-        frame.add(button, BorderLayout.SOUTH);
-
-        frame.setSize(300, 100);
+        frame.pack();
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setVisible(true);
     }
 
     public void actionPerformed(ActionEvent e) {
+
         String name = nameField.getText();
-        JOptionPane.showMessageDialog(frame, "Hello " + name + ", from Java4_5.");
+
+        String command = e.getActionCommand();
+
+        if(command.equals("Button1")) {
+            JOptionPane.showMessageDialog(frame, "Hello " + name + ", from Java4_5, Dialogue Box 1.");
+        }
+
+        else {
+            JOptionPane.showMessageDialog(frame, "Hello " + name + ", from Java4_5, Dialogue Box 2.");
+        }
     }
 
     public static void main(String[] args) {
